35b5739c9292170d13c64341ea09d50514c40415
Alier Garang <aliergarang16@gmail.com>
2022-09-07 21:12:26 -0300

    Add home.js javascript

--- setup/home.js
+++ setup/home.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.getElementById("colorValue");
+
+
+const flipColor = () => {
+
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+}
+
+const getRandomNumber = () => Math.floor(Math.random()*colors.length);
+
+btn.addEventListener("click", flipColor);
