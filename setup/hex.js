e2cf1ebd8791a27fae1906d4216916459b60e4be
Alier Garang <aliergarang16@gmail.com>
2022-09-07 22:26:24 -0300

    Add js for hex.js & complete colorFlipper

--- setup/hex.js
+++ setup/hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector('.colorValue');
+const btn = document.getElementById('btn');
+
+
+const flipHexColors = () => {
+    let hexColor = '#';
+ //hex color = 6 values
+    for(let i = 0; i < 6; i++) hexColor += hex[getRandomNumber()];
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+}
+
+const getRandomNumber = () => Math.floor(Math.random() * hex.length);
+
+btn.addEventListener('click', flipHexColors);
+
+
